982ef6024aba807b4cb7b209ebea878b929a9fb5
박민주 <pmj000907@naver.com>
2023-01-02 20:10:49 +0900

    flyLantern
    flyLantern 수정

--- ruda/src/FlyLantern.js
+++ ruda/src/FlyLantern.js
@@ -23,19 +23,14 @@ function ChooseLantern() {
     const boxRef = useRef(null);
   return (
     <>
-    {/* <div className="fly_wrapper">
-       <img className="flyLantern1" alt="flyLantern1" src="img/fly_lantern1.png" />
-       <img className="lantern1" alt="lanternA" src="img/lanternA.png" css = {lanternStyle}/>
-    </div> */}
     <div className="wrap">
     {/* <img className="lanternAfly" alt="lanternAfly" src="img/lanternAfly.png" ref={boxRef} /> */}
     <img className="flyLantern1" alt="flyLantern1" src="img/fly_lantern1.png" />
       <div className="box" ref={boxRef}>
         <motion.img
           className="lanternAfly"
-          src={"https://img.lovepik.com/free-png/20211226/lovepik-auspicious-lantern-decoration-png-image_400489436_wh300.png"}
+          src={"img/lanternAfly.png"}
           drag
-        //   top={50}
           dragDirectionLock
           dragConstraints={boxRef}
           dragSnapToOrigin
@@ -47,24 +42,4 @@ function ChooseLantern() {
     </>
   );
 }
-// const floating = keyframes`
-//     0 {
-//         transform: translateY(0);    
-//     }
-//     50% {
-//         transform: translateY(-15px);
-//     }
-//     100% {
-//         transform: translateY(0);
-//     }
-//     `
-// const lanternStyle = css`
-//     animation: ${floating} 2s ease infinite;
-//     width : 100px;
-//     height : 100px;
-//     position: absolute;
-//     bottom: 33%;
-//     right: 47.7%;
-//     z-index: 9;
-// `
 export default ChooseLantern;
--- ruda/src/signUp.js
+++ ruda/src/signUp.js
@@ -3,7 +3,10 @@ import React from "react";
 function Home() {
   return (
     <>
-      <h1>회원가입</h1>
+      <div className="sign_wrpper">
+        <h2>루다의 소원등</h2>
+        <p>본 계정은 풍등을 보관하고 공유하는 목적으로 사용됩니다.</p>
+      </div>
       
     </>
   );
