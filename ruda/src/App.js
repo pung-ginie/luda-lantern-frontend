0e86d79c3b6688296bdf0ce51dc8c95e00db511a
nayeon <20192279@sungshin.ac.kr>
2023-01-04 12:01:11 +0900

    [feat] add copyright

--- ruda/src/App.js
+++ ruda/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { Typography, Box } from "@mui/material";
 import SignUp from "./pages/signUp";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -7,8 +8,19 @@ import ChooseLantern from "./pages/ChooseLantern";
 import MakeWish from "./pages/MakeWish";
 import FlyLantern from "./pages/FlyLantern";
 import "./App.css";
+
+function Copyright() {
+  return (
+      <Typography variant="body2" color="white" align="center">
+          {"Copyright © "}
+          pungginie, {new Date().getFullYear()}
+          {"."}
+      </Typography>
+  );
+}
 function App() {
   return (
+  <div>
     <Router>
         <Switch>
           <Route path="/" exact component={Home} />
@@ -19,6 +31,13 @@ function App() {
           <Route path="/flylantern" component={FlyLantern}/>
         </Switch>
     </Router>
+    <footer>
+     <Box mt={5} className="copyright">
+         <Copyright />
+     </Box>
+    </footer>
+ </div>
+
   );
 }
 
