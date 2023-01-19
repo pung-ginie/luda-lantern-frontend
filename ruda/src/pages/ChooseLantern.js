14c3655afcb446bdaa9e780db40f4b80f7e9b0dd
YYJ-1229 <90436@naver.com>
2023-01-20 02:03:41 +0900

    [feature] create lantern

--- ruda/src/pages/ChooseLantern.js
+++ ruda/src/pages/ChooseLantern.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/ChooseLantern.css";
-import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
-function ChooseLantern() {
+import { Link, Route, Switch, useLocation, BrowserRouter as Router } from "react-router-dom";
+const ChooseLantern = () => {
+
+  const location = useLocation();
+  console.log(location);
+  
+  const [lanternType, setLantern] = useState("");
+
+  const handleType = (e) => {
+    setLantern({
+      lanternType : e.target.className,
+  });
+  };
+
   return (
-    <>
     <div className="wrapperr">
         <h2>풍등고르기</h2>
         <p>원하는 풍등 디자인을 선택하세요</p>
         <div className="lantern_wrapper">
-          <img className="lanternA" alt="lanternA" src="img/lanternA.png" tabindex="0" />
-          <img className="lanternB" alt="lanternB" src="img/lanternB.png" tabindex="1"/>
-          <img className="lanternC" alt="lanternC" src="img/lanternC.png" tabindex="2"/>
+          <img className="A" value="lanternA" alt="lanternA" src="img/lanternA.png" tabindex="0" onClick={handleType}/>
+          <img className="B" value="lanternB" alt="lanternB" src="img/lanternB.png" tabindex="1" onClick={handleType}/>
+          <img className="C" value="lanternC" alt="lanternC" src="img/lanternC.png" tabindex="2" onClick={handleType}/>
         </div>
-        <Link to="/makewish">
+
+        <Link to ={{
+          pathname : "/makewish",
+          state : {
+          lanternType : lanternType,
+          }
+        }}>
           <button className="choose_button">선택하기</button>
         </Link>
     </div>
-    </>
   );
 }
 
--- ruda/src/pages/MakeWish.js
+++ ruda/src/pages/MakeWish.js
@@ -1,22 +1,85 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import "../styles/MakeWish.css";
-import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import FlyLantern from "./FlyLantern";
-function MakeWish() {
-  return (
-    <>
-    <div className="wrapper_wish">
+import {useLocation} from "react-router-dom";
+import axios from "axios";
+
+const MakeWish = ({onCreate}) => {
+
+  const location = useLocation();
+  //console.log(location);
+
+  const lantern_number = location.state.lanternType.lanternType;
+  console.log(lantern_number);
+
+  const [toggle , setToggle] = useState(undefined);
+  const textInput = useRef();
+
+  const [state, setState] = useState({  
+    wish: "",
+    is_public: toggle,
+    lantern_number : lantern_number,
+  }); 
+
+//처음에 언디파인으로 줘서 두번 눌러야 true/false 값이 지정됨  여기 이상!!!
+  const isOpen = (e) => {
+    setToggle((toggle) => !toggle);
+    console.log(toggle);
+
+    setState({
+      ...state,
+      [e.target.name] : toggle,
+  });
+  };
+
+  const handleChangeState = (e) => {
+    setState({
+        ...state,
+        [e.target.name] : e.target.value,
+    });
+}
+  const handleSubmit = (e)=> {
+    console.log(state);
+    if(state.is_public === undefined){
+      alert("공개 여부를 선택해 주세요");
+      return;
+    }
+    if(state.wish.length < 3){
+        textInput.current.focus();
+        return;
+    }
+    onCreate(state.lantern_number, state.wish, state.is_public);
+
+    axios.post('http://luda-lantern-api-service.ap-northeast-2.elasticbeanstalk.com/v1/lantern/', { 
+      is_public : state.is_public,
+      lantern_number : state.lantern_number,
+      wish : state.wish,
+    }).then(function(response){
+      console.log(response);
+    document.location.href = "/flylantern/" + lantern_number;
+  }).catch(function(error){
+    console.log(error);
+  });
+    alert("complete");
+};
+
+  return <div className="wrapper_wish">
         <h2>소원적기</h2>
         <p>종이에 소원을 적어보세요</p>
-        <textarea className="materialize-textarea"
-          placeholder="소원을 적으면 소원이 이루어집니다."></textarea>
+        <div class="onoff-switch-container">
+          <input name="is_public" value={state.is_public} type="checkbox" id="onoff-switch1" checked={!toggle} onChange={isOpen}  />
+          <label for="onoff-switch1">{toggle === true ? "비공개": "공개" } </label> 
+        </div>
+        <textarea ref={textInput} name="wish" value={state.wish} className="materialize-textarea" 
+        placeholder="소원을 적으면 소원이 이루어집니다." onChange={handleChangeState}>
+          </textarea>
           <div className="button_wrapper">
-            <Link to="/flyLantern"><button className="wish_button">완료하기</button></Link>
-          </div>
-          
+          <button className="wish_button" onClick={handleSubmit}>완료하기</button>
+          </div> 
     </div>
-    </>
-  );
+}
+
+MakeWish.defaultProps={
+  isPublic : true //디폴트 값
 }
 
 export default MakeWish;
